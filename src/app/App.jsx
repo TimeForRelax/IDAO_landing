ebd09f342e2ab460ffb2142bd5dcb55cf8be2b7e
Artem S <ars@anadeainc.com>
2023-09-27 18:41:09 +0300

    road map

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -30,7 +30,7 @@ export const App = () => {
       <AboutUs />
       <HowItWorks />
       <RoleSystem />
-      {/* <RoadMap /> */}
+      <RoadMap />
       {/* <Tokenomics /> */}
       <StartWorkWith />
       <Footer />
